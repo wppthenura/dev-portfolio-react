1ad6d3c6bffe23d77b294dc1e93603342130e29a
wppthenura <wppulindum@gmail.com>
2025-08-18 23:43:31 +0530

    Scale adjustments

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -77,7 +77,7 @@ const TVOverlay = ({ contentType, onClose }) => {
       <Html
   position={[0.5, 0.9, 0]}
   rotation={[-0.07, Math.PI / 22, 0.01]}
-  scale={0.99} 
+  scale={1} 
   transform
   occlude
   style={{
